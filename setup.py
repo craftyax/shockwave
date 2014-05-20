af6941f863cf87db2d966b320680a864d0770f2b
Erik Axdahl <erik.axdahl@gmail.com>
2014-05-20 11:17:04 -0400

    Defined a setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='shockwave',
+      version='0.0.0',
+      author='Erik L. Axdahl',
+      packages=['shockwave'],
+      install_requires=['numpy', 'Cantera'])
