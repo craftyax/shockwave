cd254db624f4130c938ae9d8eb726e20f6d068b0
Erik Axdahl <erik.axdahl@gmail.com>
2014-05-20 10:42:43 -0400

    Added a function for printing out a summary of flow variables

--- ashockwave/classes.py
+++ ashockwave/classes.py
@@ -7,18 +7,18 @@ class flow():
 
     def __init__(self):
 
+        # Set gas properties
         self.gas = GRI30()
         self.gas.setMoleFractions('N2:0.79,O2:0.21')
-
         self.R = self.gas.cp_mass() - self.gas.cv_mass()
         self.gam = self.gas.cp_mass()/self.gas.cv_mass()
 
+        # Set flow speed of sound, Mach number, and velocity
         self.a = np.sqrt(self.gam*self.R*self.gas.temperature())
-
         self.M = 2.0
-
         self.v = self.M*self.a
         
+        # Set conservation variables
         self.setMassFlux()
         self.setMomentumFlux()
         self.setTotalEnthalpy()
@@ -34,4 +34,17 @@ class flow():
     def setMassFlux(self):
 
         self.massFlux = self.gas.density()*self.v
+
+    def flowSummary(self):
+        '''
+        Print out a summary of flow properties
+        '''
+
+        print "\nrho =", self.gas.density(), "kg/m^3"
+        print "T =", self.gas.temperature(), "K"
+        print "p =", self.gas.pressure(), "Pa"
+
+        print "\na =", self.a, "m/s"
+        print "v =", self.v, "m/s"
+        print "M =", self.M
     
